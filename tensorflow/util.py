8da94e1fb4ee8304935dd8dbb04e7c7ef1b3a281
Zhikang Li <853527789@qq.com>
2018-04-23 20:23:30 +0800

    Update util.py

--- tensorflow/util.py
+++ tensorflow/util.py
@@ -7,3 +7,15 @@ def count_params():
     """Count the number of parameters in the current TensorFlow graph """
     param_count = np.sum([np.prod(x.get_shape().as_list()) for x in tf.global_variables()])
     return param_count
+
+#usage: X, offset = jitter(X, max_jitter)
+def jitter(X, max_jitter):
+    # Randomly jitter the image a bit
+    ox, oy = np.random.randint(-max_jitter, max_jitter+1, 2)
+    X = np.roll(np.roll(X, ox, 1), oy, 2)
+    retrun X, [ox, oy]
+ 
+def unjitter(X, offset):
+    ox, oy = offset
+    # Undo the jitter
+    X = np.roll(np.roll(X, -ox, 1), -oy, 2)   
