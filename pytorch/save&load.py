7ee4a2358df420dd4f5dde9eea1be78d90132b78
Zhikang Li <feitianlzk@gmail.com>
2018-05-18 15:07:01 +0800

    Create save&load.py

---
+++ pytorch/save&load.py
@@ -0,0 +1,29 @@
+
+# view https://discuss.pytorch.org/t/saving-and-loading-a-model-in-pytorch/2610/3 to see details
+def training():
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'arch': args.arch,
+        'state_dict': model.state_dict(),
+        'best_prec1': best_prec1,
+        'optimizer' : optimizer.state_dict(),
+    }, is_best)
+        
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+        
+ def reload():
+     if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
